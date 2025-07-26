e2132f0a17cc896f33f5070ed1f2c072359b3658
Rudraksh Menon <rudrakshmenon@gmail.com>
2025-07-26 13:34:48 +0530

    added if-else problem in day01

---
+++ Day01-Intro/ifelse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ifelse {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if (n%2!=0){
+            System.out.println("Weird");
+        }else if (n%2==0 && n<5 && n>2){
+            System.out.println("Not Weird");
+        }else if (n%2==0 && n>6 && n<20){
+            System.out.println("Weird");
+        }else if (n%2==0 && n>20){
+            System.out.println("Not Weird");
+        }else{
+            System.out.println("Weird");
+        }
+    }
+}
